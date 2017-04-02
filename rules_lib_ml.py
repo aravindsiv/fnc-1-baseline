6d4b8398c27e1cd543d4539dcbf218ba64f5fc90
shar007 <snnaithanisrm@gmail.com>
2017-04-02 15:49:11 -0700

    Updates feature file to exclude 'report','reports'

--- rules_lib_ml.py
+++ rules_lib_ml.py
@@ -9,9 +9,13 @@ def common_noun_feature(hd_body_dict,max_len):
     body_noun=set()
     for i,k in enumerate(hd_body_dict["body_pos"]):
         if(k in ["PROPN","NOUN"]):
+            if(hd_body_dict["body_tok"][i] in ["reports","report","reported","reportedly"] ):
+                continue
             body_noun.add(hd_body_dict["body_tok"][i])
     for i,k in enumerate(hd_body_dict["hd_pos"]):
         if(k in ["PROPN","NOUN"]):
+            if(hd_body_dict["hd_tok"][i] in ["reports","report","reported","reportedly"] ):
+                continue
             hd_noun.add(hd_body_dict["hd_tok"][i])
     #return(len(hd_noun.intersection(body_noun))/len(hd_body_dict["hd_pos"]))
     return (len(hd_noun.intersection(body_noun)) / max_len)
@@ -21,9 +25,14 @@ def common_adjective_feature(hd_body_dict,max_len):
     body_verb=set()
     for i,k in enumerate(hd_body_dict["body_pos"]):
         if(k in ["ADV","VERB"]):
+            if(hd_body_dict["body_tok"][i] in ["reports","report","reported","reportedly"] ):
+                continue
             body_verb.add(hd_body_dict["body_tok"][i])
+
     for i,k in enumerate(hd_body_dict["hd_pos"]):
         if(k in ["ADV","VERB"]):
+            if(hd_body_dict["hd_tok"][i] in ["reports","report","reported","reportedly"] ):
+                continue
             hd_verb.add(hd_body_dict["hd_tok"][i])
     #return(len(hd_verb.intersection(body_verb))/len(hd_body_dict["hd_pos"]))
     return (len(hd_verb.intersection(body_verb)) / max_len)
