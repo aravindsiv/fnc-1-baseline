beb4833bca972e01626d915657ecb69b8c17d68d
Rohit Shinde <rohit.shinde12194@gmail.com>
2017-04-22 00:39:10 -0400

    Added validation code

--- snli.py
+++ snli.py
@@ -2,7 +2,7 @@
 import unicodedata
 from keras.models import load_model
 import keras
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Dense
 from spacy.en import English
@@ -13,8 +13,12 @@ from itertools import izip_longest
 from keras.utils import np_utils
 from sklearn import preprocessing
 import sys
+import argparse
 
 labels = {'agree':0, 'disagree':1, 'discuss':2}
+parser = argparse.ArgumentParser()
+parser.add_argument('-mode',type=str)
+args = parser.parse_args()
 
 def get_stance_matrix(answers):
 	y = []
@@ -60,6 +64,7 @@ language_model.add(Dense(3, activation='softmax'))
 
 language_model.compile(loss='categorical_crossentropy',optimizer='rmsprop')
 language_model.summary()
+
 
 headline_data = {}
 body_data = {}
@@ -113,53 +118,66 @@ holdout_dict_data = np.array(holdout_dict_data)
 
 training_dict_data[:,2] = np.array([labels[label] for label in training_dict_data[:,2]])
 
-batch_size = 100
+if args.mode == 'train':
 
-batch_ids = np.arange(0,len(training_dict_data),batch_size)
-print len(training_dict_data)
-print batch_ids
+	indices = np.linspace(0,len(training_dict_data)-1,dtype='int')
 
-indices = np.linspace(0,len(training_dict_data)-1,dtype='int')
+	for i in range(0, len(indices)-1):
+		start = indices[i]
+		end = indices[i+1]
+		timesteps_headline = 12
+		timesteps_body = 393
+		batch = training_dict_data[start:end]
+		bodies = batch[:,0]
+		headlines = batch[:,1]
+		stances = batch[:,2]
 
-for i in range(0, len(indices)-1):
-	start = indices[i]
-	end = indices[i+1]
-	timesteps_headline = 12
-	timesteps_body = 393
-	batch = training_dict_data[start:end]
-	bodies = batch[:,0]
-	headlines = batch[:,1]
-	stances = batch[:,2]
+		headlines_batch = get_timeseries_nlp(headlines, nlp, timesteps_headline)
+		bodies_batch = get_timeseries_nlp(bodies, nlp, timesteps_body)
 
-	headlines_batch = get_timeseries_nlp(headlines, nlp, timesteps_headline)
-	bodies_batch = get_timeseries_nlp(bodies, nlp, timesteps_body)
+		x = np.hstack((headlines_batch, bodies_batch))
+		# y = [int(i) for i in stances]
+		y = []
+		for i in stances:
+			if int(i) == 0:
+				y.append([1,0,0])
+			elif int(i) == 1:
+				y.append([0,1,0])
+			elif int(i) == 2:
+				y.append([0,0,1])
+		loss = language_model.train_on_batch(x,y)
+		print "Iteration Done"
 
-	x = np.hstack((headlines_batch, bodies_batch))
-	# y = [int(i) for i in stances]
-	y = []
-	for i in stances:
-		if int(i) == 0:
-			y.append([1,0,0])
-		elif int(i) == 1:
-			y.append([0,1,0])
-		elif int(i) == 2:
-			y.append([0,0,1])
-	loss = language_model.train_on_batch(x,y)
-	print "Iteration Done"
+	language_model.save('language.h5')
 
-language_model.save('language.h5')
+if args.mode == 'validate':
+	language_model = load_model('language.h5')
+	indices = np.linspace(0, len(holdout_dict_data)-1, dtype='int')
 
-# print len(training_ids)
-# for i in xrange(16):
-# 	for headlines, bodies, stances in training_dict:
-# 		timesteps_headline = 12 #len(nlp(headlines[-1]))
-# 		timesteps_body = 393 #len(nlp(bodies[-1]))
-# 		headlines_batch = get_timeseries_nlp(headlines, nlp, timesteps_headline)
-# 		bodies_batch = get_timeseries_nlp(bodies, nlp, timesteps_body)
-# 		print headlines_batch.shape, bodies_batch.shape
-# 		x = np.hstack((headlines_batch, bodies_batch))
-# 		# x = x.reshape(1,x.shape[0],x.shape[1])
-# 		y = get_stance_matrix(stances, labelencoder)
-# 		print y.shape
-# 		loss = language_model.train_on_batch(x , y)
-# 		print "Iteration done"
+	for i in range(0, len(indices)-1):
+		start = indices[i]
+		end = indices[i+1]
+		timesteps_headline = 12
+		timesteps_body = 393
+		batch = training_dict_data[start:end]
+		bodies = batch[:,0]
+		headlines = batch[:,1]
+		stances = batch[:,2]
+
+		headlines_batch = get_timeseries_nlp(headlines, nlp, timesteps_headline)
+		bodies_batch = get_timeseries_nlp(bodies, nlp, timesteps_body)
+
+		x = np.hstack((headlines_batch, bodies_batch))
+
+		y = []
+		for i in stances:
+			if int(i) == 0:
+				y.append([1,0,0])
+			elif int(i) == 1:
+				y.append([0,1,0])
+			elif int(i) == 2:
+				y.append([0,0,1])
+
+		prediction = language_model.predict(x)
+		print prediction
+		break
