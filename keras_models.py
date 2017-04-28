d21e3b2c92cbb36e9298452cc5e3d8b7b4e58fdd
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2017-04-28 19:38:44 -0400

    Ready for training

--- keras_models.py
+++ keras_models.py
@@ -14,72 +14,75 @@ sentence_hidden_layer_size = 300
 activation = 'relu'
 RNN = None
 dropout_rate = 0.2
-batch_size = 256
-num_epochs = 50
+batch_size = 
+num_epochs = 64
 patience = 5
-
 
 if __name__ == "__main__":
 	pp = PreProcessor()
 	pp.preprocess_keras()
 	embedding_matrix = pp.get_embedding_matrix()
 
-	########################
+	parser = argparse.ArgumentParser()
 
-	data_dict = pp.make_data_keras(fold)
+	parser.add_argument('-fold',help='fold to train the model on',default='0')
+
+	args = vars(parser.parse_args())
+
+	data_dict = pp.make_data_keras(args['fold'])
 	bodies, headlines, labels = data_dict["train"]
 	bodies_t, headlines_t, labels_t = data_dict["test"]
 	max_seq_length = data_dict["max_seq_length"]
 
-embed = Embedding(len(pp.word_index)+1,embedding_dim,weights=[embedding_matrix],input_length=pp.max_seq_length)
+	embed = Embedding(len(pp.word_index)+1,embedding_dim,weights=[embedding_matrix],input_length=max_seq_length)
 
-SumEmbeddings = keras.layers.core.Lambda(lambda x: K.sum(x,axis=1),output_shape=(sentence_hidden_layer_size,))
+	SumEmbeddings = keras.layers.core.Lambda(lambda x: K.sum(x,axis=1),output_shape=(sentence_hidden_layer_size,))
 
-translate = TimeDistributed(Dense(sentence_hidden_layer_size,activation=activation))
+	translate = TimeDistributed(Dense(sentence_hidden_layer_size,activation=activation))
 
-body = Input(shape=(pp.max_seq_length,))
-headline = Input(shape=(pp.max_seq_length,))
+	body = Input(shape=(pp.max_seq_length,))
+	headline = Input(shape=(pp.max_seq_length,))
 
-bod = embed(body)
-head = embed(headline)
+	bod = embed(body)
+	head = embed(headline)
 
-bod = translate(bod)
-head = translate(head)
+	bod = translate(bod)
+	head = translate(head)
 
-rnn = SumEmbeddings # Change this line if you want an RNN. Refer to Smerity's code.
-bod = rnn(bod)
-head = rnn(head)
-bod = BatchNormalization()(bod)
-head = BatchNormalization()(head)
+	rnn = SumEmbeddings # Change this line if you want an RNN. Refer to Smerity's code.
+	bod = rnn(bod)
+	head = rnn(head)
+	bod = BatchNormalization()(bod)
+	head = BatchNormalization()(head)
 
-joint = merge([bod,head],mode='concat')
+	joint = merge([bod,head],mode='concat')
 
-joint = Dropout(dropout_rate)(joint)
+	joint = Dropout(dropout_rate)(joint)
 
-for i in range(3):
-  joint = Dense(2 * sentence_hidden_layer_size,activation=activation)(joint) # No weight regularization being done.
-  joint = Dropout(dropout_rate)(joint)
-  joint = BatchNormalization()(joint)
+	for i in range(2):
+	  joint = Dense(2 * sentence_hidden_layer_size,activation=activation)(joint) # No weight regularization being done.
+	  joint = Dropout(dropout_rate)(joint)
+	  joint = BatchNormalization()(joint)
 
-pred = Dense(labels.shape[1],activation='softmax')(joint)
+	pred = Dense(labels.shape[1],activation='softmax')(joint)
 
-model = Model(input = [body,headline],output=pred)
-model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
+	model = Model(input = [body,headline],output=pred)
+	model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
 
-model.summary()
+	model.summary()
 
-_, tmpfn = tempfile.mkstemp()
-# Save the best model during validation and bail out of training early if we're not improving
-callbacks = [EarlyStopping(patience=patience), ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True)]
+	_, tmpfn = tempfile.mkstemp()
+	# Save the best model during validation and bail out of training early if we're not improving
+	callbacks = [EarlyStopping(patience=patience), ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True)]
 
-model.fit([bodies, headlines], labels,batch_size=batch_size,epochs=num_epochs,callbacks=callbacks)
+	model.fit([bodies, headlines], labels,batch_size=batch_size,epochs=num_epochs,callbacks=callbacks)
 
-# Restore the best found model during validation
-model.load_weights(tmpfn)
+	# Restore the best found model during validation
+	model.load_weights(tmpfn)
 
-loss, acc = model.evaluate([bodies_t, headlines_t],labels_t)
-print "Test loss: %s, accuracy: %s" %(loss, acc)
+	loss, acc = model.evaluate([bodies_t, headlines_t],labels_t)
+	print "Test loss: %s, accuracy: %s" %(loss, acc)
 
-# Save final model, just to be safe
-model.save("SumRNN.h5")
+	# Save final model, just to be safe
+	model.save("SumRNN_"+str(args['fold'])+".h5")
 
