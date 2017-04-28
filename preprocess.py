a80d5ff32345a2e4f9bfcb35b0f735fb11d218c4
shar007 <snnaithanisrm@gmail.com>
2017-04-28 16:19:39 -0700

    separated training and test capabilities

--- preprocess.py
+++ preprocess.py
@@ -31,8 +31,7 @@ def preprocess_func(dataset):
     mapping_id={}
     vocab_tokens={}
     for i,k in enumerate(dataset.stances):
-        if(i==100):
-            break
+
         print(i)
         buff={}
         buff["id"]=k['Body ID']
@@ -43,12 +42,13 @@ def preprocess_func(dataset):
         if(buff["id"] in mapping_id):
             buff["body_pos"]=data_proc[mapping_id[buff["id"]]]["body_pos"]
             buff["body_tok"]=data_proc[mapping_id[buff["id"]]]["body_tok"]
-            buff["body_entities"]=data_proc[mapping_id[buff["id"]]]["body_entities"]
+            #buff["body_entities"]=data_proc[mapping_id[buff["id"]]]["body_entities"]
         else:
             mapping_id[buff["id"]]=i
             buff["body_pos"] =[]
             buff["body_tok"]=[]
             doc_body = nlp(buff["body"])
+            """
             entity_list=[]
             buff_list=[]
             sent_list=[]
@@ -58,15 +58,18 @@ def preprocess_func(dataset):
             buff_list=st.tag_sents(sent_list)
             for m in buff_list:
                 entity_list.extend(m)
-            for word in doc_body:
-                buff["body_pos"].append(word.pos_)
-                buff["body_tok"].append(word.text)
             entity_dict={entity:set() for entity in ["PERSON", "ORGANIZATION", "LOCATION"]}
-            
             for pair in entity_list:
                 if(pair[1] in ["PERSON", "ORGANIZATION", "LOCATION"]):
                      entity_dict[pair[1]].add(pair[0])
-            buff["body_entities"]=entity_dict     
+            buff["body_entities"]=entity_dict 
+            """             
+            for word in doc_body:
+                buff["body_pos"].append(word.pos_)
+                buff["body_tok"].append(word.text)
+            
+            
+    
         doc_hd=nlp(buff["hd"])
 
         buff["hd_pos"]=[]
@@ -74,6 +77,7 @@ def preprocess_func(dataset):
         for word in doc_hd:
             buff["hd_pos"].append(word.pos_)
             buff["hd_tok"].append(word.text)
+        """    
         entity_dict={entity:set() for entity in ["PERSON", "ORGANIZATION", "LOCATION"]}
         #entity_list=st.tag(buff["hd_tok"])
         entity_list=[]
@@ -89,16 +93,56 @@ def preprocess_func(dataset):
         for pair in entity_list:
             if(pair[1] in ["PERSON", "ORGANIZATION", "LOCATION"]):
                 entity_dict[pair[1]].add(pair[0])
-        buff["hd_entities"]=entity_dict     
+        buff["hd_entities"]=entity_dict
+        """
         vocab_tokens[i]=[tok.lower().strip() for tok in set(buff["hd_tok"]).union(set(buff["body_tok"]))]    
         data_proc.append(buff)    
     return output,data_proc,vocab_tokens
 
+
+
 output,data_proc,vocab_tokens=preprocess_func(dataset)
-pickle.dump(output,open("labels100.pk","wb"))
-pickle.dump(data_proc,open("dataset_pickle100.pk","wb"))
-with open('data.json100', 'w') as fp:
-    json.dump(vocab_tokens, fp)
+training_ids=[int(k.strip()) for k in open("/home/ubuntu/ml_project/fnc-1-baseline/splits/training_ids.txt","rb")]
+test_ids=[int(k.strip()) for k in open("/home/ubuntu/ml_project/fnc-1-baseline/splits/hold_out_ids.txt","rb")]
+
+train_data=[]
+training_label=[]
+test_data=[]
+test_label=[]
+
+for i,k in enumerate(data_proc):
+        if(k["id"] in training_ids):
+            train_data.append(k)
+            training_label.append(output[i])
+            continue
+        elif(k["id"] in test_ids):
+            test_data.append(k)
+            test_label.append(output[i])
+            continue        
+                                     
+#pickle.dump(output,open("labels100.pk","wb"))
+#pickle.dump(data_proc,open("dataset_pickle100.pk","wb"))
+pickle.dump(training_label,open("training_label.pk","wb"))
+pickle.dump(train_data,open("train_data.pk","wb"))
+pickle.dump(test_label,open("test_label.pk","wb"))
+pickle.dump(test_data,open("test_data.pk","wb"))
+#with open('data.json100', 'w') as fp:
+#    json.dump(vocab_tokens, fp)                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+ 
+
+
+
 
 
 
