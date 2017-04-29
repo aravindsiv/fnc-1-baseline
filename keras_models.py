2e81851ba4f30ad84a221670f389a94e0fd14cd3
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2017-04-29 15:03:02 -0400

    Added rnn

--- keras_models.py
+++ keras_models.py
@@ -4,19 +4,20 @@ import keras
 import keras.backend as K
 import argparse
 import tempfile
+
 from keras.models import Sequential
-from keras.layers import Embedding, Dense, Input, TimeDistributed, merge, Dropout, BatchNormalization
+from keras.layers import Embedding, Dense, Input, TimeDistributed, merge, Dropout, BatchNormalization, recurrent
 from keras.models import Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 embedding_dim = 300
 sentence_hidden_layer_size = 300
 activation = 'relu'
-RNN = None
 dropout_rate = 0.2
-batch_size = 512
+batch_size = 64
 num_epochs = 10
 patience = 5
+L2 = 4e-6
 
 if __name__ == "__main__":
 	pp = PreProcessor()
@@ -26,6 +27,7 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 
 	parser.add_argument('-fold',help='fold to train the model on',default='0')
+	parser.add_argument('-rnn',help='no | lstm | gru',default='no')
 
 	args = vars(parser.parse_args())
 
@@ -33,6 +35,16 @@ if __name__ == "__main__":
 	bodies, headlines, labels = data_dict["train"]
 	bodies_t, headlines_t, labels_t = data_dict["test"]
 	max_seq_length = data_dict["max_seq_length"]
+
+	if args['rnn'] == 'no':
+		RNN = None
+	elif args['rnn'] == 'lstm':
+		RNN = recurrent.LSTM 
+	elif args ['rnn'] == 'gru':
+		RNN = recurrent.GRU
+	else:
+		print "Invalid arg for rnn"
+		RNN = None
 
 	embed = Embedding(len(pp.tokenizer.word_index)+1,embedding_dim,weights=[embedding_matrix],input_length=max_seq_length)
 
@@ -49,7 +61,7 @@ if __name__ == "__main__":
 	bod = translate(bod)
 	head = translate(head)
 
-	rnn = SumEmbeddings # Change this line if you want an RNN. Refer to Smerity's code.
+	rnn = SumEmbeddings if not RNN else RNN(return_sequences=False,output_dim=sentence_hidden_layer_size)
 	bod = rnn(bod)
 	head = rnn(head)
 	bod = BatchNormalization()(bod)
@@ -59,8 +71,8 @@ if __name__ == "__main__":
 
 	joint = Dropout(dropout_rate)(joint)
 
-	for i in range(2):
-	  joint = Dense(2 * sentence_hidden_layer_size,activation=activation)(joint) # No weight regularization being done.
+	for i in range(3):
+	  joint = Dense(2 * sentence_hidden_layer_size,activation=activation,W_regularizer=l2(L2) if L2 else None)(joint) # No weight regularization being done.
 	  joint = Dropout(dropout_rate)(joint)
 	  joint = BatchNormalization()(joint)
 
@@ -71,18 +83,18 @@ if __name__ == "__main__":
 
 	model.summary()
 
-	_, tmpfn = tempfile.mkstemp()
+	# _, tmpfn = tempfile.mkstemp()
 	# Save the best model during validation and bail out of training early if we're not improving
-	callbacks = [EarlyStopping(patience=patience), ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True)]
+	# callbacks = [EarlyStopping(patience=patience), ModelCheckpoint(tmpfn, save_best_only=True, save_weights_only=True)]
 
-	model.fit([bodies, headlines], labels,batch_size=batch_size,epochs=num_epochs,callbacks=callbacks)
+	model.fit([bodies, headlines], labels,batch_size=batch_size,epochs=num_epochs)#,callbacks=callbacks)
 
 	# Restore the best found model during validation
-	model.load_weights(tmpfn)
+	# model.load_weights(tmpfn)
 
 	loss, acc = model.evaluate([bodies_t, headlines_t],labels_t)
 	print "Test loss: %s, accuracy: %s" %(loss, acc)
 
 	# Save final model, just to be safe
-	model.save("SumRNN_"+str(args['fold'])+".h5")
+	model.save("models/SumRNN_"+str(args['fold'])+".h5")
 
