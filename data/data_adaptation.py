47d6337502b848154a11b452460ebc93b4941e36
Saurabh Gokhale <sg1183@scarletmail.rutgers.edu>
2017-04-30 01:05:15 -0400

    Used enumeration to get index.

--- data/data_adaptation.py
+++ data/data_adaptation.py
@@ -15,14 +15,14 @@ def read_json_data(src_folder="data/"):
     """
     with open(src_folder+file_name_train) as data_file:
         data_rows = json.load(data_file)
-        train_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"])] for row in data_rows]
+        train_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"]), idx] for idx, row in enumerate(data_rows)]
 
     with open(src_folder+file_name_test) as data_file:
         data_rows = json.load(data_file)
-        test_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"])] for row in data_rows]
+        test_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"]), idx] for idx, row in enumerate(data_rows)]
 
     with open(src_folder+file_name_dev) as data_file:
         data_rows = json.load(data_file)
-        dev_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"])] for row in data_rows]
+        dev_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"]), idx] for idx, row in enumerate(data_rows)]
 
     return np.array(train_data), np.array(test_data), np.array(dev_data)
