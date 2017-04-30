aaf38ad6eab151c5d0b759dd2f158aaa07d8c4f8
Saurabh Gokhale <sg1183@scarletmail.rutgers.edu>
2017-04-29 23:29:55 -0400

    Updated code for preprocessing task for data adaptation

--- data/data_adaptation.py
+++ data/data_adaptation.py
@@ -7,27 +7,22 @@ file_name_test = "snli_1.0_test.json"
 file_name_dev = "snli_1.0_dev.json"
 
 
-def read_json_data():
+def read_json_data(src_folder="data/"):
     """
     Read json data (per preprocess logic) for train, test and dev json.
     :param file_name: 
     :return: train_data, test_data, dev_data
     """
-    with open(file_name_train) as data_file:
+    with open(src_folder+file_name_train) as data_file:
         data_rows = json.load(data_file)
         train_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"])] for row in data_rows]
 
-    with open(file_name_test) as data_file:
+    with open(src_folder+file_name_test) as data_file:
         data_rows = json.load(data_file)
         test_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"])] for row in data_rows]
 
-    with open(file_name_dev) as data_file:
+    with open(src_folder+file_name_dev) as data_file:
         data_rows = json.load(data_file)
         dev_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"])] for row in data_rows]
 
     return np.array(train_data), np.array(test_data), np.array(dev_data)
-
-# train_d, test_d, dev_d = read_json_data()
-# print(train_d[0])
-# print(test_d[0])
-# print(dev_d[0])
