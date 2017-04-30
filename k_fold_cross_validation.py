8fd95d16225a9bf9e402778cf212a90bdb2398d5
Saurabh Gokhale <sg1183@scarletmail.rutgers.edu>
2017-04-30 19:38:14 -0400

    Added k-fold cross validation script.

--- k_fold_cross_validation.py
+++ k_fold_cross_validation.py
@@ -1,74 +1,42 @@
+import argparse
 import sys
-import numpy as np
 
-from sklearn.ensemble import GradientBoostingClassifier
-from feature_engineering import refuting_features, polarity_features, hand_features, gen_or_load_feats
-from feature_engineering import word_overlap_features
-from utils.dataset import DataSet
-from utils.generate_test_splits import kfold_split, get_stances_for_folds
-from utils.score import report_score, LABELS, score_submission
-
-from utils.system import parse_params, check_version
+from preprocess import PreProcessor
+from keras_models import test_model
+from keras.layers import recurrent
 
 if __name__ == "__main__":
-    check_version()
-    parse_params()
 
-    d = DataSet()
-    folds, hold_out = kfold_split(d, n_folds=10)
-    fold_stances, hold_out_stances = get_stances_for_folds(d,folds, hold_out)
+    parser = argparse.ArgumentParser()
 
-    Xs = dict()
-    ys = dict()
+    parser.add_argument('-fold', help='fold to train the model on', default='0')
+    parser.add_argument('-rnn', help='no | lstm | lstm_1 | gru', default='no')
 
-    """
-    NOTE: Treat like black-box
-    
-    # Load/Precompute all features now
-    X_holdout,y_holdout = generate_features(hold_out_stances, d, "holdout")
-    for fold in fold_stances:
-        Xs[fold], ys[fold] = generate_features(fold_stances[fold], d, str(fold))
-    """
-    # X_holdout, y_holdout =
-    # Xs[fold], ys[fold] =
+    args = vars(parser.parse_args())
 
-    best_score = 0
-    best_fold = None
+    k_fold = args['fold']
 
-    # Classifier for each fold
-    for fold in fold_stances:
-        ids = list(range(len(folds)))
-        print("Ids:", ids)
-        print("Fold:", fold)
-        print(ids[fold])
-        del ids[fold]
+    if args['rnn'] == 'no':
+        RNN = None
+        fprefix = "SumRNN"
+    elif args['rnn'] == 'lstm':
+        RNN = recurrent.LSTM
+        fprefix = "lstm"
+    elif args['rnn'] == 'gru':
+        RNN = recurrent.GRU
+        fprefix = "gru"
+    elif args['rnn'] == 'lstm_1':
+        RNN = recurrent.LSTM
+    else:
+        print "Invalid arg for rnn"
+        RNN = None
+        sys.exit(0)
 
-        X_train = np.vstack(tuple([Xs[i] for i in ids]))
-        y_train = np.hstack(tuple([ys[i] for i in ids]))
+    pp = PreProcessor()
+    pp.preprocess_keras()
+    pp.preprocess_stageone()
 
-        X_test = Xs[fold]
-        y_test = ys[fold]
+    data_dict = pp.make_data_keras(k_fold)
 
-        """
-        clf = GradientBoostingClassifier(n_estimators=200, random_state=14128, verbose=True)
-        clf.fit(X_train, y_train)
-        """
-
-        predicted = [LABELS[int(a)] for a in clf.predict(X_test)]
-        actual = [LABELS[int(a)] for a in y_test]
-
-        fold_score, _ = score_submission(actual, predicted)
-        max_fold_score, _ = score_submission(actual, actual)
-
-        score = fold_score/max_fold_score
-
-        print("Score for fold " + str(fold) + " was - " + str(score))
-        if score > best_score:
-            best_score = score
-            best_fold = clf
-
-    # Run on Holdout set and report the final score on the holdout set
-    predicted = [LABELS[int(a)] for a in best_fold.predict(X_holdout)]
-    actual = [LABELS[int(a)] for a in y_holdout]
-
-    report_score(actual, predicted)
+    model_file = "models/" + fprefix + "_" + str(k_fold) + ".h5"
+    test_model(model_file, data_dict)
