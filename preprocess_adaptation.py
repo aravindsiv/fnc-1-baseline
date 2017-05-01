ac30db4f7c5918bff1fba54a5a2fdbbbe34a5cd2
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2017-04-30 22:10:06 -0400

    Clean up preprocess for adaptation

--- preprocess_adaptation.py
+++ preprocess_adaptation.py
@@ -4,16 +4,33 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from collections import defaultdict
 from preprocess_ import preprocess_func
-from data.data_adaptation import read_json_data
 
 import csv
+import json
 import numpy as np
 import unicodedata
 
+file_name_train = "snli_1.0_train.json"
+file_name_test = "snli_1.0_test.json"
+file_name_dev = "snli_1.0_dev.json"
 
 def _normalize(text):
     return unicodedata.normalize('NFKD', text.decode('utf8')).encode('ascii', 'ignore')
 
+def read_json_data(src_folder="data/"):
+    with open(src_folder+file_name_train) as data_file:
+        data_rows = json.load(data_file)
+        train_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"]), idx] for idx, row in enumerate(data_rows)]
+
+    with open(src_folder+file_name_test) as data_file:
+        data_rows = json.load(data_file)
+        test_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"]), idx] for idx, row in enumerate(data_rows)]
+
+    with open(src_folder+file_name_dev) as data_file:
+        data_rows = json.load(data_file)
+        dev_data = [[_normalize(row["sentence1"]), _normalize(row["sentence2"]), _normalize(row["gold_label"]), idx] for idx, row in enumerate(data_rows)]
+
+    return np.array(train_data), np.array(test_data), np.array(dev_data)
 
 class PreProcessor:
 
