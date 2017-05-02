5c0d8a849e16630bc159f8d11b728efb0f139aae
Saurabh Gokhale <sg1183@scarletmail.rutgers.edu>
2017-05-01 21:17:36 -0400

    Update code.

--- test_fnc.py
+++ test_fnc.py
@@ -5,4 +5,8 @@ from sklearn.metrics import pairwise
 if __name__ == "__main__":
     pp = PreProcessor()
     model_name = "lstm.h5"
+    data_dict = {}
+    data_dict["bodies"] = pp.test_data[:,0]
+    data_dict["headline"] = pp.test_data[:, 1]
+    data_dict["labels"] = pp.test_data[:, 2]
     test_model("models_snli/"+model_name, pp.test_data)
