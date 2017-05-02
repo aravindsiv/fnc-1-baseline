de8a49a87551268afb09ec3f03eb469bad0fcd82
Saurabh Gokhale <sg1183@scarletmail.rutgers.edu>
2017-05-01 22:13:46 -0400

    Testing all folds together

--- test_fnc.py
+++ test_fnc.py
@@ -6,9 +6,23 @@ from keras.models import Sequential, load_model
 if __name__ == "__main__":
     pp = PreProcessor()
     model_name = "lstm.h5"
-    data_dict = {}
+    data_dict_0 = {}
+    data_dict_1 = {}
+    data_dict_2 = {}
+    data_dict_3 = {}
+    data_dict_4 = {}
     pp.preprocess_keras()
-    data_dict = pp.make_data_keras(4)
+    data_dict_0 = pp.make_data_keras(0)
+    data_dict_1 = pp.make_data_keras(1)
+    data_dict_2 = pp.make_data_keras(2)
+    data_dict_3 = pp.make_data_keras(3)
+    data_dict_4 = pp.make_data_keras(4)
+
+    data_dict = data_dict_0.update(data_dict_1)
+    data_dict = data_dict.update(data_dict_2)
+    data_dict = data_dict.update(data_dict_3)
+    data_dict = data_dict.update(data_dict_4)
+
     bodies, headlines, labels = data_dict["train"]
     bodies_t, headlines_t, labels_t = data_dict["test"]
 
