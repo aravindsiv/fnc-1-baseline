554536f8dd4654f3a3914d8160a79153457938e4
Saurabh Gokhale <sg1183@scarletmail.rutgers.edu>
2017-05-01 22:57:21 -0400

    Updated code.

--- test_fnc.py
+++ test_fnc.py
@@ -2,16 +2,12 @@ from preprocess import PreProcessor
 from keras_models import test_model
 from sklearn.metrics import pairwise
 from keras.models import Sequential, load_model
+from collections import defaultdict
 
 if __name__ == "__main__":
     pp = PreProcessor()
     model_name = "lstm.h5"
-    data_dict_0 = {}
-    data_dict_1 = {}
-    data_dict_2 = {}
-    data_dict_3 = {}
-    data_dict_4 = {}
-    data_dict = {}
+    data_dict = defaultdict()
     pp.preprocess_keras()
     data_dict_0 = pp.make_data_keras(0)
     data_dict_1 = pp.make_data_keras(1)
@@ -19,7 +15,17 @@ if __name__ == "__main__":
     data_dict_3 = pp.make_data_keras(3)
     data_dict_4 = pp.make_data_keras(4)
 
-    data_dict = data_dict_0.update(data_dict_1.update(data_dict_2.update(data_dict_3.update(data_dict_4))))
+    super_dict = defaultdict(set)
+    dicts = [data_dict_0, data_dict_1, data_dict_2, data_dict_3, data_dict_4]
+
+    for d in dicts:
+        for k, v in d.iteritems():
+            super_dict[k].add(v)
+
+    data_dict = super_dict
+    #data_dict_0 = dict(data_dict_0)
+    #data_dict_0.update(data_dict_1.update(data_dict_2.update(data_dict_3.update(data_dict_4))))
+    #data_dict = data_dict_0.copy()
     #data_dict = data_dict.update(data_dict_1)
     #data_dict = data_dict.update(data_dict_2)
     #data_dict = data_dict.update(data_dict_3)
